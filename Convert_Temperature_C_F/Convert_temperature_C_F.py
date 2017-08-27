cb1d4d78ddc0f984d09765e8b6e55eaaa9cc60f7
peaches12 <vlaz1999@yahoo.com>
2017-08-26 21:54:55 -0400

    Comments are added

--- Convert_Temperature_C_F/Convert_temperature_C_F.py
+++ Convert_Temperature_C_F/Convert_temperature_C_F.py
@@ -1,11 +1,29 @@
-# convert the temperature to C or F
-def to_c(temp):
-    C =5/9*(temp-32)
-    C=round(C,1)
-    return(C)
+#Date: Aug 23,2017
+#Project Name: Convert Temperature
+#Author: Olga Lazarenko
+#Description: the program contains two functions:
+#               the function to_c(temp) converts the temperature rom F to C
+#               the function to_f(temp) converst the temperature from C to F
 
-def to_f(temp):
-    F = 32+5/9*(temp)
-    F=round(F,1)
-    return(F)
+
+print('to convert from F to C call the function to_c(temp)')
+print('to convert from C to F call the function to_f(temp)')
+print()
+
+def to_c(temp):#this function converts the temperature from F to C
+    #temp is the input temperature value from the user
+    
+    C =5/9*(temp-32)#the formula to caluclate 
+    C=round(C,1)#we want the result with one decimal number 
+    print(str(temp)+'F will be '+str(C)+'C')#the result of calculation will be returned from the function
+    return
+
+def to_f(temp): #this function converts the temperature from C to F
+    #temp is the input temperature values from the user 
+
+    F = 32+5/9*(temp)# the formula to calculate 
+    F=round(F,1)# the result will we rounded to one decimal number
+    print(str(temp)+'C  will be '+str(F)+ 'F')#the result is retured from the function
+    return
+
 
--- Convert_Time/Convert_to_seconds.py
+++ Convert_Time/Convert_to_seconds.py
@@ -1,8 +1,23 @@
-#the program will create a function to calculate/convert the time into seconds
+#Date: Aug 1, 2017
+#Program Name: Convert Time
+#Author: Olga Lazarenko
+#Description: the program uses the  function convert_to_seconds(hours,minutes,seconds)
+#               to convert the time into seconds
+
+print('to convert the input time into seconds,')
+print('call the function convert_to_seconds(hours,minutes,seconds)')
+print()
+print('---------------------------')
 
 def convert_to_seconds(hours,minutes,seconds):
-    result =hours*60*60+minutes*60+seconds
-    return result
+    #the fuction takes the input in hours, min, seconds from the user
+    #and converts to seconds
+    print()
+    result =hours*60*60+minutes*60+seconds #the formula for conversion
+    print('it will be '+ str(result) + ' seconds')
+    return
+
+
 
 
 
